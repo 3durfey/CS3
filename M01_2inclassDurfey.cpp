a5bfc54c61edd11b6cdebe458b059d13101884f2
pdurfey <3durfey@gmail.com>
2023-02-06 21:00:47 -0500

    finished m01_2

--- M01_2inclassDurfey.cpp
+++ M01_2inclassDurfey.cpp
@@ -16,10 +16,19 @@ int main()
     cout << *numberPointer << endl;
     //an array name is a pointer with the pointer pointing at the very first memory address of the array, which can be incrementing in memory to get the rest of the array
     int arrayExample[4] = {1,3,4,5};
+    int *ptr = arrayExample;
     //first element in array through pointer
-    cout << "first element in array through pointer " << *arrayExample << endl;
-    //second element using array 
-    cout << "second element using array " << *arrayExample + 1 << endl;
+    cout << "first element in array through pointer " << *ptr << endl;
+    //second element using array, also using pointer arithmetic. Pointer arithmetic is incrementing a pointer value. Incrementing it by 1 will go to the next memory address (the next 4 bytes).
+    cout << "second element using array " << *ptr++ << endl;
 
+    //another coding example using pointer arithmetic, the time complexity would be O(n) since it would grow with the size of the array
+    int arrayLength = 5;
+    ptr = arrayExample;
+    for(int x = 1; x < arrayLength; x++)
+    {
+        cout << "Array number " << x << " value: " << *ptr << endl;
+        ptr++;
+    }
 
 }
